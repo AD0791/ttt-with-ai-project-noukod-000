6cf31f96951df5e960623ad98b90b9e585aa192b
Alexandro Disla <alexandrodisla@hotmail.com>
2019-05-28 12:54:12 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -48,7 +48,7 @@ class Game
     player = current_player
     current_move = player.move(@board)
     if !@board.valid_move?(current_move)
-      turn
+      turn # recursion
     else
       puts "Turn: #{@board.turn_count+1}\n"
       @board.display
